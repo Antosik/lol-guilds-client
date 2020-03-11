e6e9a4e26b8852a9591258067f0bdcd8a7fe37eb
Antosik <grig_anton96@mail.ru>
2020-03-11 23:54:06 +0300

    feat(main): remove top menu

--- app/ui/window.ts
+++ app/ui/window.ts
@@ -1,11 +1,11 @@
-import type { BrowserViewConstructorOptions } from "electron";
+import type { BrowserWindowConstructorOptions } from "electron";
 
 import { BrowserWindow } from "electron";
 import { join as joinPath } from "path";
 
 export class Window extends BrowserWindow {
-  constructor(options: BrowserViewConstructorOptions = {}) {
-    const settings = {
+  constructor(options: BrowserWindowConstructorOptions = {}) {
+    const settings: BrowserWindowConstructorOptions = {
       title: "Guilds Client",
       width: 800,
       height: 600,
@@ -18,6 +18,7 @@ export class Window extends BrowserWindow {
     };
     super(settings);
 
+    this.removeMenu();
     this.loadFile(joinPath(__dirname, "..", "index.html"));
 
     this.once("ready-to-show", () => {
@@ -26,4 +27,4 @@ export class Window extends BrowserWindow {
   }
 }
 
-export const createWindow = (options: BrowserViewConstructorOptions = {}): Window => new Window(options);
+export const createWindow = (options: BrowserWindowConstructorOptions = {}): Window => new Window(options);

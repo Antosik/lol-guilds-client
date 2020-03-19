71ca2e637f0700fda0389fd3d77b831dca207d27
Antosik <grig_anton96@mail.ru>
2020-03-19 15:14:09 +0300

    feat: GuildsAPI for stats & rating

--- main/src/client.ts
+++ main/src/client.ts
@@ -79,6 +79,26 @@ export class MainApplication {
         if (!this._guildsClient) return null;
         return this._guildsClient.api.getSeasons();
       });
+      this._rpc.setHandler("guilds:season", async (season_id: number) => {
+        if (!this._guildsClient) return null;
+        return this._guildsClient.api.getSeason(season_id);
+      });
+      this._rpc.setHandler("guilds:rating:season", async (season_id: number) => {
+        if (!this._guildsClient) return null;
+        return this._guildsClient.api.getTopClubsForSeasonWithId(season_id);
+      });
+      this._rpc.setHandler("guilds:rating:stage", async (season_id: number, stage_id: number) => {
+        if (!this._guildsClient) return null;
+        return this._guildsClient.api.getTopClubsForStageWithId(stage_id, season_id);
+      });
+      this._rpc.setHandler("guilds:stats:season", async (season_id: number, club_id: number) => {
+        if (!this._guildsClient) return null;
+        return this._guildsClient.getGuildSeasonStats(season_id, club_id);
+      });
+      this._rpc.setHandler("guilds:stats:stage", async (season_id: number, stage_id: number, club_id: number) => {
+        if (!this._guildsClient) return null;
+        return this._guildsClient.getGuildStageStats(stage_id, season_id, club_id);
+      });
     }
   }
   // #endregion

5132eb5fc6e127f3e9ca3330030ba0cf8abf95ab
Antosik <grig_anton96@mail.ru>
2020-03-22 01:38:35 +0300

    fix: guildStore - don't reset every time

--- renderer/src/store/guild.ts
+++ renderer/src/store/guild.ts
@@ -9,9 +9,16 @@ export interface IGuildStore {
 
 function createGuildStore() {
   const getInitialStore = (): IGuildStore => ({ guild: null, members: [] });
-  const { set, subscribe, update } = writable<IGuildStore>(getInitialStore());
+  const { subscribe, update } = writable<IGuildStore>(getInitialStore());
 
-  const setGuildData = (guild: IKeyValue): void => set({ ...getInitialStore(), guild });
+  const setGuildData = (guild: IKeyValue): void => update(store => {
+    if (store?.guild?.id !== guild.id) {
+      return { ...getInitialStore(), guild };
+    } else {
+      return { ...store, guild };
+    }
+  });
+
   const setMembers = (members: IKeyValue[]): void => update(store => ({ ...store, members }));
   const setMemberStatus = (member: IKeyValue): void => update(store => {
     const storeWithoutMember = store.members.filter(m => member.name !== m.name);

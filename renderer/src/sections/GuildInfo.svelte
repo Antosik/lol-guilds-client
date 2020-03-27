9646a9a98f37eff6956d4bfcc602c5c6828a7f4c
Antosik <grig_anton96@mail.ru>
2020-03-27 16:27:57 +0300

    rework: update styles for guild page

--- renderer/src/sections/GuildInfo.svelte
+++ renderer/src/sections/GuildInfo.svelte
@@ -1,12 +1,9 @@
 <script>
-  import { link } from "svelte-spa-router";
-
   export let guild = undefined;
 </script>
 
 <style>
-  .guild-info__top,
-  .guild-info__descriptions {
+  .guild-info__top {
     margin: 12px 0;
     display: flex;
     flex-wrap: wrap;
@@ -21,36 +18,27 @@
   }
 
   .recruitment-status {
-    color: #f4c2c2;
+    --status-color-light: var(--status-away);
+    --status-color-normal: transparent;
+
+    color: var(--status-color-light);
   }
   .recruitment-status .status__circle {
     display: inline-block;
-    background: #a70202;
-    border: 2px solid #f4c2c2;
+    color: var(--status-color-normal);
+    border: 2px solid var(--status-color-light);
     border-radius: 50%;
     width: 8px;
     height: 8px;
     margin-right: 4px;
   }
   .recruitment-status--active {
-    color: #42dd88;
-  }
-  .recruitment-status--active .status__circle {
-    background: #02a64b;
-    border: 2px solid #42dd88;
-  }
-
-  .guild-info__short_description,
-  .guild-info__long_description {
-    width: calc(50% - 10px);
-  }
-  .guild-info__short_description {
-    margin-right: 20px;
+    --status-color-light: var(--status-active-light);
+    --status-color-normal: var(--status-active-normal);
   }
 </style>
 
 <div class="guild-info__top flex-center">
-
   <div class="guild-info__top__item">Глава: {guild.owner.summoner_name}</div>
 
   <div class="guild-info__top__item">
@@ -65,24 +53,4 @@
       Набор {guild.is_hiring ? 'открыт' : 'закрыт'}
     </span>
   </div>
-
 </div>
-{#if guild.short_description || guild.long_description}
-  <div class="guild-info__descriptions">
-
-    {#if guild.short_description}
-      <div class="guild-info__short_description">
-        <h3>Краткое описание</h3>
-        <div class="guild-info__description">{guild.short_description}</div>
-      </div>
-    {/if}
-
-    {#if guild.long_description}
-      <div class="guild-info__long_description">
-        <h3>Описание</h3>
-        <div class="guild-info__description">{guild.long_description}</div>
-      </div>
-    {/if}
-
-  </div>
-{/if}

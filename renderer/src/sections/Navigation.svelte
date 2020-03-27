d603f776b21d81812c0b8c05cb0a4bdb96247a1b
Antosik <grig_anton96@mail.ru>
2020-03-27 17:12:38 +0300

    feat: move styles for active links

--- renderer/src/sections/Navigation.svelte
+++ renderer/src/sections/Navigation.svelte
@@ -6,41 +6,26 @@
 <style>
   a {
     padding: 4px 8px;
-    margin: 2px;
+    margin: 2px; 
     text-transform: uppercase;
     font-size: 14px;
-  }
-  :global(.subpages_navigation a.active) {
-    color: var(--main-primary);
-  }
-  :global(.subpages_navigation a.active:after) {
-    content: "";
-    position: absolute;
-    bottom: 0px;
-    left: 0;
-    right: 0;
-
-    margin: 0 auto;
-    width: 75%;
-    height: 2px;
-    background: var(--main-primary);
   }
 </style>
 
 <nav class="subpages_navigation">
   <ul class="flex-center">
     <li>
-      <a href="/client/" class="flex-center" use:link use:active>Главная</a>
+      <a href="/client/" class="flex-center use-active" use:link use:active>Главная</a>
     </li>
     <li>
-      <a href="/client/guild/" class="flex-center" use:link use:active>
+      <a href="/client/guild/" class="flex-center use-active" use:link use:active>
         Моя гильдия
       </a>
     </li>
     <li>
       <a
         href="/client/rating/"
-        class="flex-center"
+        class="flex-center use-active"
         use:link
         use:active={'/client/rating/*'}>
         Рейтинг

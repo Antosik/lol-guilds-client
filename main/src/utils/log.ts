8df755c440f68076fe70e071c106cde97163b990
Antosik <grig_anton96@mail.ru>
2020-04-06 20:34:02 +0300

    feat: divide log files to levels

--- main/src/utils/log.ts
+++ main/src/utils/log.ts
@@ -1,4 +1,10 @@
 import Log, { error, debug, info } from "electron-log";
+import { join as joinPath } from "path";
+
+Log.transports.file.resolvePath = function(variables, message) {
+  const level = message?.level ?? "debug";
+  return joinPath(variables.libraryDefaultDir, `${level}.${variables.fileName}`);
+};
 
 export { error as logError, debug as logDebug, info as logInfo };
 export default Log;

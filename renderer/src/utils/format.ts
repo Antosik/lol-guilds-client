22f662ec7a5ec7bfc14a739253edcb2860ad46fc
Antosik <grig_anton96@mail.ru>
2020-11-06 19:18:27 +0300

    misc: optimize date-fns import

--- renderer/src/utils/format.ts
+++ renderer/src/utils/format.ts
@@ -1,21 +1,24 @@
 import type { Locale } from "date-fns";
 
-import { format, formatDistance, formatDistanceToNow } from "date-fns";
-import { ru, enUS } from "date-fns/locale";
+import format from "date-fns/format";
+import formatDistance from "date-fns/formatDistance";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import ru from "date-fns/locale/ru";
+import enUS from "date-fns/locale/en-US";
 
 
-function getLocale(locale: "ru" | "en"): Locale {
+function getDateLocale(locale: "ru" | "en"): Locale {
   return locale === "ru" ? ru : enUS;
 }
 
 export function formatDateDistanceToNow(date: string | Date, locale: "ru" | "en"): string {
-  return formatDistanceToNow(new Date(date), { locale: getLocale(locale), addSuffix: true });
+  return formatDistanceToNow(new Date(date), { locale: getDateLocale(locale), addSuffix: true });
 }
 
 export function formatDateDistanceTo(from: string | Date, to: string | Date, locale: "ru" | "en"): string {
-  return formatDistance(new Date(from), new Date(to), { locale: getLocale(locale), addSuffix: true });
+  return formatDistance(new Date(from), new Date(to), { locale: getDateLocale(locale), addSuffix: true });
 }
 
 export function formatDate(date: string | Date, locale: "ru" | "en"): string {
-  return format(new Date(date), "d MMMM", { locale: getLocale(locale) });
+  return format(new Date(date), "d MMMM", { locale: getDateLocale(locale) });
 }
